e2f824a237931cebdbc4161673bb662466a2bda5
tkydia <thiernoabda16@gmail.ccom>
2024-02-29 21:10:03 +0000

    Affichage des videos

--- screens/Home.js
+++ screens/Home.js
@@ -1,98 +1,58 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Image,
-  FlatList,
-  StyleSheet,
-  Dimensions,
-  TouchableOpacity,
-} from "react-native";
+import { View, FlatList } from "react-native";
 import { ref, listAll, getDownloadURL } from "firebase/storage";
 import { Video } from "expo-av";
 import { storage } from "../firebaseConfig";
 
 export default function Home() {
-  const [medias, setMedias] = useState([]);
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    const fetchMedia = async () => {
+    const fetchVideos = async () => {
       const listRef = ref(storage, "Medias/");
 
       try {
         const result = await listAll(listRef);
-        const mediaUrls = await Promise.all(
+        const videoUrls = await Promise.all(
           result.items.map(async (itemRef) => {
             const url = await getDownloadURL(itemRef);
-            const type =
-              itemRef.contentType && itemRef.contentType.startsWith("video/")
-                ? "video"
-                : "image";
+
             return {
               url,
-              type,
             };
           })
         );
 
-        setMedias(mediaUrls);
+        setVideos(videoUrls);
       } catch (error) {
-        console.error("Error listing files:", error);
+        console.error("Error listing videos:", error);
       }
     };
 
-    fetchMedia();
+    fetchVideos();
   }, []);
 
   return (
     <View style={{ flex: 1 }}>
       <FlatList
-        numColumns={2}
-        data={medias}
-        keyExtractor={(item, index) => index.toString()}
+        data={videos}
+        keyExtractor={(item) => item.url}
         renderItem={({ item }) => (
-          // Pour rendre les images cliquable
-          <TouchableOpacity>
-            <View style={styles.itemContainer}>
-              {item.type === "image" ? (
-                <Image
-                  source={{ uri: item.url }}
-                  style={styles.mediaThumbnail}
-                />
-              ) : (
-                <Video
-                  source={{ uri: item.url }}
-                  rate={1.0}
-                  volume={1.0}
-                  isMuted={false}
-                  resizeMode="cover"
-                  shouldPlay
-                  style={styles.mediaThumbnail}
-                  useNativeControls
-                />
-              )}
-            </View>
-          </TouchableOpacity>
+          <Video
+            source={{ uri: item.url }}
+            rate={1.0}
+            volume={1.0}
+            isMuted={false}
+            resizeMode="cover"
+            shouldPlay
+            style={{ width: "48%", height: 100 }}
+            useNativeControls
+          />
         )}
+        numColumns={2}
+        contentContainerStyle={{ gap: 2 }}
+        columnWrapperStyle={{ gap: 2 }}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    padding: 10,
-  },
-  itemContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    margin: 5,
-  },
-  mediaThumbnail: {
-    width: Dimensions.get("window").width / 2 - 10, // Adjusted width to fit two items in a row
-    height: Dimensions.get("window").width / 2 - 10, // Adjusted height to fit two items in a row
-    borderRadius: 8,
-  },
-});
